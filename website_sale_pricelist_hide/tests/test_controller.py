854a329e7fd93f74b36de0ad4229bf571078f880
Brett Wood <bmw95@shaw.ca>
2016-08-01 19:57:37 -0700

    [FIX] website_sale_pricelist_hide: Enable tests for empty context and present context.

--- website_sale_pricelist_hide/tests/test_controller.py
+++ website_sale_pricelist_hide/tests/test_controller.py
@@ -2,7 +2,7 @@
 # © 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-# import mock
+import mock
 from openerp.tests.common import TransactionCase
 from openerp.addons.website_sale_pricelist_hide.controllers.main import (
     WebsiteSale
@@ -22,44 +22,40 @@ class TestController(TransactionCase):
             'name': 'Test List',
         })
 
-    # @mock.patch('%s.http' % imp_core)
-    # @mock.patch('%s.http' % imp_main)
-    # def test_get_search_domain_context(self, local_mk, core_mk):
-    #     """ It should extend the search domain with the context pricelist """
-    #     core_mk.request.website.sale_product_domain.return_value = []
-    #     local_mk.request.env.context.get.return_value = self.pricelist_id.id
-    #     res = self.controller._get_search_domain(None, None, None)
-    #     expect = ('item_ids.pricelist_id', '=', self.pricelist_id.id)
-    #     found = False
-    #     for item in res:
-    #         if expect == item:
-    #             found = True
-    #             break
-    #     self.assertTrue(
-    #         found,
-    #         'Did not find pricelist in domain. Expect %s to be in %s' % (
-    #             expect, res,
-    #         )
-    #     )
-    #
-    # @mock.patch('%s.http.request.website' % imp_core)
-    # @mock.patch('%s.http.request.env.context' % imp_main)
-    # def test_get_search_domain_no_context(self, local_mk, core_mk):
-    #     """ It should extend the search domain with the default pricelist """
-    #     core_mk.request.website.sale_product_domain.return_value = []
-    #     local_mk.request.env.context.get.return_value = False
-    #     with mock.patch.object(self.controller, 'get_pricelist') as plist_mk:
-    #         plist_mk.return_value = self.pricelist_id
-    #         res = self.controller._get_search_domain(None, None, None)
-    #         expect = ('item_ids.pricelist_id', '=', self.pricelist_id.id)
-    #         found = False
-    #         for item in res:
-    #             if expect == item:
-    #                 found = True
-    #                 break
-    #         self.assertTrue(
-    #             found,
-    #             'Did not find pricelist in domain. Expect %s to be in %s' % (
-    #                 expect, res,
-    #             )
-    #         )
+    @mock.patch('%s.request' % imp_core)
+    @mock.patch('%s.http' % imp_main)
+    def test_get_search_domain_context(self, local_mk, core_mk):
+        """Test extend search domain with the context pricelist"""
+        core_mk.website.sale_product_domain.return_value = []
+        local_mk.request.env.context.__getitem__.return_value = \
+            self.pricelist_id.id
+
+        expect = ('item_ids.pricelist_id', '=', self.pricelist_id.id)
+        result = self.controller._get_search_domain(None, None, None)
+
+        self.assertIn(
+            expect,
+            result,
+            'Did not find pricelist in domain. Expect %s to be in %s' % (
+                expect, result,
+            )
+        )
+
+    @mock.patch('%s.request' % imp_core)
+    @mock.patch('%s.http' % imp_main)
+    def test_get_search_domain_no_context(self, local_mk, core_mk):
+        """Test extend search domain with the default pricelist"""
+        core_mk.website.sale_product_domain.return_value = []
+        core_mk.website.get_current_pricelist.return_value = self.pricelist_id
+        local_mk.request.env.context.get.return_value = False
+
+        expect = ('item_ids.pricelist_id', '=', self.pricelist_id.id)
+        result = self.controller._get_search_domain(None, None, None)
+
+        self.assertIn(
+            expect,
+            result,
+            'Did not find pricelist in domain. Expect %s to be in %s' % (
+                expect, result,
+            )
+        )
